7a5cfa2f5ce205343a2a7e542e46043c4a6427ef
Danilo Abranches <daniloeabranches@gmail.com>
2020-11-30 22:29:46 -0300

    Desenvolvimento de tela de entrada do app

--- app/src/main/java/com/cuidedacidade/features/main/MainActivity.kt
+++ app/src/main/java/com/cuidedacidade/features/main/MainActivity.kt
@@ -62,6 +62,10 @@ class MainActivity : AppCompatActivity() {
                 R.id.PendingRequestsFragment, R.id.AllRequestsFragment -> fab.show()
                 else -> fab.hide()
             }
+            when (destination.id) {
+                R.id.StartupFragment -> supportActionBar?.hide()
+                else -> supportActionBar?.show()
+            }
         }
     }
 
