fd01da7a5e75b65a88766456cca12b4c36dd23c7
Danilo Abranches <daniloeabranches@gmail.com>
2021-02-11 08:19:51 -0300

    Criação de testes

--- app/src/main/java/com/cuidedacidade/utils/AlertDialogUtils.kt
+++ app/src/main/java/com/cuidedacidade/utils/AlertDialogUtils.kt
@@ -1,8 +1,6 @@
 package com.cuidedacidade.utils
 
 import android.content.Context
-import android.content.DialogInterface
-import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
 import com.cuidedacidade.R
 
@@ -12,12 +10,4 @@ object AlertDialogUtils {
             .setPositiveButton(
                 context.getString(R.string.ok)
             ) { _, _ -> }.create().show()
-
-    fun showAlert(
-        context: Context,
-        @StringRes messageId: Int,
-        listener: DialogInterface.OnClickListener
-    ) =
-        AlertDialog.Builder(context).setMessage(messageId)
-            .setPositiveButton(context.getString(R.string.ok), listener).create().show()
 }
